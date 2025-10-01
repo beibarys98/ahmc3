f8e419dc712a59458bd056e6e15d1a9363b7d159
beibarys <beibarys.mukhammedyarov@alumni.nu.edu.kz>
2025-10-01 14:11:28 +0500

    1 oct changes 17

--- frontend/views/test/view.php
+++ frontend/views/test/view.php
@@ -170,11 +170,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'class' => ActionColumn::className(),
                     'template' => '{view} {update} {delete}',
                     $urlCreator = function ($action, UserTest $model, $key, $index, $column) {
-                        $userCycle = UserCycle::find()->andWhere(['user_id' => $model->user_id])->one();
-                        if ($userCycle) {
-                            return Url::toRoute(['user-cycle/' . $action, 'id' => $userCycle->id]);
-                        }
-                        return '#'; // или null, или другую ссылку по умолчанию
+                        return Url::toRoute(['user/' . $action, 'id' => $model->user_id]);
                     },
                 ]
             ],
